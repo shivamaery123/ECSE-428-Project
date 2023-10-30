6e168588c217c5dabfc73354e28246182c4c4901
Mariobouzakhm <mariobouzakhm02@gmail.com>
2023-10-30 01:20:04 -0400

    Added User Login

--- backend/controllers/user_controller.js
+++ backend/controllers/user_controller.js
@@ -80,4 +80,38 @@ const get_user = async(req,res) => {
   }
 }
 
-module.exports = { createUser, get_all_users, get_user };
+const login = async(req, res) => {
+  try {
+    const  { username, password} = req.body;
+    var user = await User.findOne({
+      where: {
+        username: username,
+        password: password
+      }
+    });
+
+    if(user == null) {
+      res.status(404).json({
+        status: "Failed",
+        message: `Could not find user with username/password`
+      });
+    }
+    else {
+      res.status(201).json({
+        status: "Success",
+        message: "User logged in successfully.",
+        data: {
+          username: user.username
+        },
+      });
+    }
+  }
+  catch (err) {
+    res.status(400).json({
+      status: "Failed",
+      message: `User was not successfuly logged in, ${err}`,
+    });
+  }
+}
+
+module.exports = { createUser, get_all_users, get_user, login};
