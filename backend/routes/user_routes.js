357e2915d23c800bd29a24e78695687c73f073ec
Saif-Shahin <90293080+Saif-Shahin@users.noreply.github.com>
2023-10-30 19:23:25 -0400

    added new routes for the game histroy methods in user_routes.js

--- backend/routes/user_routes.js
+++ backend/routes/user_routes.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const user_router = express.Router();
-const { createUser, get_all_users, get_user } = require("../controllers/user_controller");
+const { createUser, 
+       get_all_users, 
+       get_user,
+      addGameToHistory,
+      removeGameFromHistory,
+      retreiveGameHistory} = require("../controllers/user_controller");
 
 user_router.post("/create", createUser);
 
@@ -9,5 +14,10 @@ user_router.get("/all", get_all_users);
 user_router.get("/user", get_user)
 // /user?id={id}
 // /user?email={email}
+
+user_router.post("/game/add", addGameToHistory);         
+user_router.delete("/game/remove", removeGameFromHistory); 
+user_router.delete("/game/clear", ClearGameHistory);      
+user_router.get("/game/history", retrieveGameHistory);   
 
 module.exports = user_router;
