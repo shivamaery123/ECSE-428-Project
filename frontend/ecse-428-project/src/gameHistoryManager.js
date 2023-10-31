5d755c5b421f0828d41764860f876d1e2fdad039
Saif-Shahin <90293080+Saif-Shahin@users.noreply.github.com>
2023-10-30 20:09:38 -0400

    Update gameHistoryManager.js

--- frontend/ecse-428-project/src/gameHistoryManager.js
+++ frontend/ecse-428-project/src/gameHistoryManager.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { UserContext } from './UserContext'; //SEE COMMENT ON LINE 6
 
 const gameHistoryManager = () => {
     const { userId } = useContext(UserContext); //NOTE: THIS LINE NEEDS TO BE CHANGED BASED ON HOW THE USER'S INFO IS STORED WHEN THEY LOG IN.
