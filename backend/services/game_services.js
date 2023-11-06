418b38e52af065cf420033f52b92666c09d662ce
Asma Gandour <asma.gandour@mail.mcgill.ca>
2023-11-06 12:48:31 -0500

    removed prev tests

--- backend/services/game_services.js
+++
@@ -1,7 +0,0 @@
-const Game = require('../models/Game')
-
-function create(game_name, game_creator, game_type) {
-  return Game.create(game_name, game_creator, game_type);
-}
-
-module.exports = { create };
