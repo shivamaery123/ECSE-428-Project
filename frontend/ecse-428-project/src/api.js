08aae941b8d8cd4bae892bc32048402db840bed0
Saif-Shahin <90293080+Saif-Shahin@users.noreply.github.com>
2023-11-06 13:57:46 -0500

    Update api.js

--- frontend/ecse-428-project/src/api.js
+++ frontend/ecse-428-project/src/api.js
@@ -24,3 +24,39 @@ export const signUpUser = async (username, email, password) => {
         return { success: false, error: error.response.data || error.message };
     }
 };
+
+export const getGameHistory = async (userId) => {
+    try {
+        const response = await axios.get(`/user?user_id=${userId}`);
+        return { success: true, data: response.data };
+    } catch (error) {
+        return { success: false, error: error.response.data || error.message };
+    }
+};
+
+export const addGameToHistory = async (userId, game) => {
+    try {
+        const response = await axios.post('/addGameToHistory', { user_id: userId, game });
+        return { success: true, data: response.data };
+    } catch (error) {
+        return { success: false, error: error.response.data || error.message };
+    }
+};
+
+export const removeGameFromHistory = async (userId, game) => {
+    try {
+        const response = await axios.post('/removeGameFromHistory', { user_id: userId, game });
+        return { success: true, data: response.data };
+    } catch (error) {
+        return { success: false, error: error.response.data || error.message };
+    }
+};
+
+export const clearGameHistory = async (userId) => {
+    try {
+        await axios.post('/ClearGameHistory', { user_id: userId });
+        return { success: true };
+    } catch (error) {
+        return { success: false, error: error.response.data || error.message };
+    }
+};
