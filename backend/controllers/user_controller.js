fd2c431e9f884fcd6fbf9cb4807bd59f4b5542d1
Saif-Shahin <90293080+Saif-Shahin@users.noreply.github.com>
2023-11-06 13:32:56 -0500

    Update user_controller.js

--- backend/controllers/user_controller.js
+++ backend/controllers/user_controller.js
@@ -203,5 +203,150 @@ const login = async(req, res) => {
     });
   }
 }
+const addGameToHistory= async (req, res) => {
 
-module.exports = { registerUser, get_all_users, get_user, deleteUser, modify_user, login};
+  try{
+    //getting userID and game from the request
+    const userId= req.body.user_id;
+    const game= req.body.game; //NOTE: MIGHT NEED TO BE CHANGED BASED ON THE GAME MODEL
+
+    //Fetching the respective user from the database
+    const user= await User, findOne({ where: { user_id: userId } });
+
+    if (!user) {
+            return res.status(404).json({
+                status: "Failed",
+                message: "User not found."
+            });
+        }
+
+    let gameHistory = JSON.parse(user.game_history); 
+    gameHistory.push(game);
+
+    await user.update({ game_history: JSON.stringify(gameHistory) });
+
+    res.status(200).json({
+            status: "Success",
+            message: "Game added to history successfully."
+        });
+       } catch (err) { // Catch any errors that occur during execution
+        // Respond with a 400 status and a failure message, including the error details
+        res.status(400).json({
+            status: "Failed",
+            message: `Error: ${err}`
+        });
+    }
+};
+
+const removeGameFromHistory = async (req, res) => {
+    try {
+        const userId = req.body.user_id;
+        const game = req.body.game;
+
+        const user = await User.findOne({ where: { user_id: userId } });
+
+        if (!user) {
+            return res.status(404).json({
+                status: "Failed",
+                message: "User not found."
+            });
+        }
+
+        let gameHistory = JSON.parse(user.game_history);
+
+        if (gameHistory.length === 0) {
+            return res.status(404).json({
+                status: "Failed",
+                message: "No games are in your game history."
+            });
+        }
+
+        const gameIndex = gameHistory.indexOf(game);
+
+        if (gameIndex === -1) {
+            return res.status(404).json({
+                status: "Failed",
+                message: "Game not found in history."
+            });
+        }
+
+        gameHistory.splice(gameIndex, 1);
+        await user.update({ game_history: JSON.stringify(gameHistory) });
+
+        res.status(200).json({
+            status: "Success",
+            message: "Game removed from history successfully."
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: "Failed",
+            message: `Error: ${err}`
+        });
+    }
+};
+
+const clearGameHistory = async (req, res) => {
+    try {
+        const userId = req.body.user_id;
+
+        const user = await User.findOne({ where: { user_id: userId } });
+
+        if (!user) {
+            return res.status(404).json({
+                status: "Failed",
+                message: "User not found."
+            });
+        }
+
+        await user.update({ game_history: "[]" });
+
+        res.status(200).json({
+            status: "Success",
+            message: "Game history cleared successfully."
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: "Failed",
+            message: `Error: ${err}`
+        });
+    }
+};
+
+const retrieveGameHistory = async (req, res) => {
+    try {
+        const userId = req.query.user_id;
+
+        const user = await User.findOne({ where: { user_id: userId } });
+
+        if (!user) {
+            return res.status(404).json({
+                status: "Failed",
+                message: "User not found."
+            });
+        }
+
+        const gameHistory = JSON.parse(user.game_history);
+
+        res.status(200).json({
+            status: "Success",
+            data: gameHistory
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: "Failed",
+            message: `Error: ${err}`
+        });
+    }
+};
+
+
+
+module.exports = { registerUser, get_all_users, 
+                  get_user, 
+                  deleteUser, 
+                  modify_user, 
+                  login,
+                  addGameToHistory, 
+                  removeGameFromHistory,
+                  clearGameHistory,
+                  retrieveGameHistory };
