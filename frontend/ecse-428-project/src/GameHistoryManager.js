7851e6df9ed18312b885682f7d88a4a08f395c43
Saif-Shahin <90293080+Saif-Shahin@users.noreply.github.com>
2023-11-06 14:01:16 -0500

    Update GameHistoryManager.js

--- frontend/ecse-428-project/src/GameHistoryManager.js
+++ frontend/ecse-428-project/src/GameHistoryManager.js
@@ -1,38 +1,100 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { UserContext } from './UserContext'; //SEE COMMENT ON LINE 6
+import React, { useState, useContext } from 'react';
+import { UserContext } from './UserContext'; //see comment on line 6
+import { getGameHistory, addGameToHistory, removeGameFromHistory, clearGameHistory } from './api';
 import './GameHistoryManager.css';
 
-const gameHistoryManager = () => {
-    const { userId } = useContext(UserContext); //NOTE: THIS LINE NEEDS TO BE CHANGED BASED ON HOW THE USER'S INFO IS STORED WHEN THEY LOG IN.
+const GameHistoryManager = () => {
+    const { userId } = useContext(UserContext); // not sure how to implement this line
     const [game, setGame] = useState('');
     const [gameHistory, setGameHistory] = useState([]);
     const [message, setMessage] = useState('');
 
-    const handleRetrieveGameHistory = () => {
+    const handleRetrieveGameHistory = async () => {
         if (userId) {
-            axios.get(`/user?user_id=${userId}`)
-                .then(response => {
-                    setGameHistory(JSON.parse(response.data.user.game_history));
-                })
-                .catch(error => {
-                    console.error('Error retrieving game history:', error);
-                });
+            const response = await getGameHistory(userId);
+            if (response.success) {
+                setGameHistory(response.data.user.game_history);
+            } else {
+                console.error('Error retrieving game history:', response.error);
+            }
         }
     };
 
-      const handleAddGame = () => {
-        axios.post('/addGameToHistory', { user_id: userId, game })
-            .then(response => {
-                // Update the game history state after successfully adding a game
-                setGameHistory(prevHistory => [...prevHistory, game]);
-                setMessage("Game added successfully.");
-            })
-            .catch(error => {
-                console.error('Error adding game:', error);
-            });
+    const handleAddGame = async () => {
+        const response = await addGameToHistory(userId, game);
+        if (response.success) {
+            setGameHistory(prevHistory => [...prevHistory, game]);
+            setMessage("Game added successfully.");
+        } else {
+            console.error('Error adding game:', response.error);
+        }
     };
 
+
+    const handleRemoveGame = async () => {
+        if (gameHistory.length === 0) {
+            setMessage("No games in your history to remove.");
+            return;
+        }
+        if (!gameHistory.includes(game)) {
+            setMessage("Game not found in history.");
+            return;
+        }
+
+        const response = await removeGameFromHistory(userId, game);
+        if (response.success) {
+            setGameHistory(prevHistory => prevHistory.filter(g => g !== game));
+            setMessage("Game removed successfully.");
+        } else {
+            console.error('Error removing game:', response.error);
+            setMessage("Failed to remove game.");
+        }
+    };
+
+    const handleClearHistory = async () => {
+        if (gameHistory.length === 0) {
+            setMessage("No games in your history to clear.");
+            return;
+        }
+
+        const response = await clearGameHistory(userId);
+        if (response.success) {
+            setGameHistory([]);
+            setMessage("Game history cleared successfully.");
+        } else {
+            console.error('Error clearing game history:', response.error);
+            setMessage("Failed to clear game history.");
+        }
+    };
+    
+    return (
+        <div>
+            {/* Removed the User ID input as it's now obtained from context */}
+            <div>
+                <label>Game:</label>
+                <input type="text" value={game} onChange={e => setGame(e.target.value)} />
+                <button onClick={handleAddGame}>Add Game</button>
+                <button onClick={handleRemoveGame}>Remove Game</button>
+            </div>
+
+            <button onClick={handleRetrieveGameHistory}>Retrieve Game History</button>
+            <button onClick={handleClearHistory}>Clear Game History</button>
+
+            <div>
+                <h3>Game History:</h3>
+                <ul>
+                    {gameHistory.map((g, index) => <li key={index}>{g}</li>)}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+
+export default gameHistoryManager;
+
+
+'''
     const handleRemoveGame = () => {
         if (gameHistory.length === 0) {
             setMessage("No games are in your game history.");
@@ -69,29 +131,4 @@ const gameHistoryManager = () => {
                 console.error('Error clearing game history:', error);
             });
     };
-
-    return (
-        <div>
-            {/* Removed the User ID input as it's now obtained from context */}
-            <div>
-                <label>Game:</label>
-                <input type="text" value={game} onChange={e => setGame(e.target.value)} />
-                <button onClick={handleAddGame}>Add Game</button>
-                <button onClick={handleRemoveGame}>Remove Game</button>
-            </div>
-
-            <button onClick={handleRetrieveGameHistory}>Retrieve Game History</button>
-            <button onClick={handleClearHistory}>Clear Game History</button>
-
-            <div>
-                <h3>Game History:</h3>
-                <ul>
-                    {gameHistory.map((g, index) => <li key={index}>{g}</li>)}
-                </ul>
-            </div>
-        </div>
-    );
-};
-
-
-export default gameHistoryManager;
+'''
